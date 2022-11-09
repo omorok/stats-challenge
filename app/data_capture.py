593147db0d5cbcd1070ca142f4ddbb20ceef2352
omorokin <matamba41@gmail.com>
2022-11-09 11:22:14 -0500

    feat: creating building stat method

--- app/data_capture.py
+++ app/data_capture.py
@@ -11,3 +11,10 @@ class DataCapture:
 
         self.numbers.append(number)
         self.numbers_count[number] += 1
+    
+    def build_stats(self):
+       
+        lowest = [0]*self.max_value
+        for i in range(1, len(self.numbers_count)):
+            lowest[i] = lowest[i-1] + self.numbers_count[i-1]
+        return Stats(self.numbers_count, lowest, len(self.numbers))
